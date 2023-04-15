7c9aea22600419d17c53a6a5a9e52f68e6639b30
Allan Galera <allangalera@gmail.com>
2023-04-15 18:38:25 -0300

    feat: Replace image slider with a library

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -8,6 +8,7 @@ import {
 import { RiSystemExternalLinkLine } from "solid-icons/ri";
 import { isEmpty, isNil, isNotNil } from "ramda";
 import { Image, Button } from "@kobalte/core";
+import { createSlider } from "solid-slider";
 
 export const ListItem: Component<ItemWithID> = ({
   id,
@@ -16,42 +17,21 @@ export const ListItem: Component<ItemWithID> = ({
   images,
   price,
 }) => {
-  const [currentImage, setCurrentImage] = createSignal(0);
+  const [slider, { current, next, prev, moveTo }] = createSlider();
 
-  const isFirst = createMemo(() => currentImage() === 0);
+  const isFirst = createMemo(() => current() === 0);
   const isLast = createMemo(() => {
     if (isNil(images)) return false;
-    console.log(name, currentImage() === images.length - 1);
-    return currentImage() === images.length - 1;
+    console.log(name, current() === images.length - 1);
+    return current() === images.length - 1;
   });
-
-  const prev = () => {
-    if (isNil(images)) return;
-    const prevImg = Math.max(currentImage() - 1, 0);
-    document.querySelector(`#${id}-${prevImg}`)?.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "end",
-    });
-    setCurrentImage(prevImg);
-  };
-  const next = () => {
-    if (isNil(images)) return;
-    const nextImg = Math.min(currentImage() + 1, images.length - 1);
-    document.querySelector(`#${id}-${nextImg}`)?.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "end",
-    });
-    setCurrentImage(nextImg);
-  };
   return (
     <div class="rounded bg-slate-300 dark:bg-slate-900 p-4 border-slate-400 dark:border-slate-800 hover:border-slate-500 dark:hover:border-slate-700 border border-solid flex flex-col gap-4 grid-cols-[auto_1fr]">
       {isNotNil(images) && !isEmpty(images) ? (
         <div class="rounded dark:bg-slate-800 w-full aspect-square relative">
           {isFirst() ? null : (
             <Button.Root
-              class="absolute top-[50%] translate-y-[-50%] translate-x-1 left-0 p-2 bg-slate-900 rounded opacity-50"
+              class="absolute z-10 top-[50%] translate-y-[-50%] translate-x-1 left-0 p-2 bg-slate-900 rounded opacity-50"
               onClick={prev}
             >
               <RiSystemArrowLeftSFill />
@@ -59,25 +39,26 @@ export const ListItem: Component<ItemWithID> = ({
           )}
           {isLast() ? null : (
             <Button.Root
-              class="absolute top-[50%] translate-y-[-50%] translate-x-[-4px] right-0 p-2 bg-slate-900 rounded opacity-50"
+              class="absolute z-10 top-[50%] translate-y-[-50%] translate-x-[-4px] right-0 p-2 bg-slate-900 rounded opacity-50"
               onClick={next}
             >
               <RiSystemArrowRightSFill />
             </Button.Root>
           )}
           <div
-            class="h-full overflow-x-auto overflow-y-hidden whitespace-nowrap snap-x scroll-smooth grid grid-rows-1"
+            use:slider
+            class="flex relative aspect-square overflow-hidden items-center rounded w-full h-full"
             style={{
-              "grid-template-columns": `repeat(${images.length}, 100%)`,
+              "grid-auto-columns": `repeat(${images.length}, 100%)`,
             }}
           >
             <For each={images}>
               {(image, i) => (
                 <div
                   id={`${id}-${i()}`}
-                  class="w-full h-full flex items-center justify-center snap-center"
+                  class="flex items-center justify-center max-h-full w-full h-full"
                 >
-                  <Image.Root class="flex max-h-full">
+                  <Image.Root class="max-h-full flex">
                     <Image.Img
                       class="max-h-full object-contain"
                       src={image}
