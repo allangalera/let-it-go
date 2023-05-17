2b0d8434fb4babdf944aff29152bdf62becdbdfa
Allan Galera <allangalera@gmail.com>
2023-05-17 19:27:17 -0300

    feat: Update packages

--- sst.config.ts
+++ sst.config.ts
@@ -12,10 +12,10 @@ export default {
     app.stack(function Site({ stack }) {
       const site = new SolidStartSite(stack, "site", {
         buildCommand: "pnpm build",
-        customDomain: {
-          hostedZone: "allaude.com",
-          domainName: "desapego.allaude.com",
-        },
+        // customDomain: {
+        //   hostedZone: "allaude.com",
+        //   domainName: "desapego.allaude.com",
+        // },
         environment: {
           WEBINY_API_KEY: process.env.WEBINY_API_KEY,
           WEBINY_API_URL: process.env.WEBINY_API_URL,
